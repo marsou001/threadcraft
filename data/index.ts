dd6f12dbf821a62878ba13712f105e13d1c6e6ca
marouane <marsou431@gmail.com>
2025-03-27 14:34:29 +0000

    create landing page

---
+++ data/index.ts
@@ -0,0 +1,30 @@
+export const features = [
+  {
+    title: "Twitter Threads",
+    // icon: <SiX className="w-10 h-10 mb-4 text-black" />,
+    icon: "/icons/x.png",
+    iconAltTitle: "X logo",
+    description: "Generate compelling Twitter threads that engage your audience and boost your reach.",
+  },
+  {
+    title: "Instagram Captions",
+    icon: "/icons/instagram.png",
+    iconAltTitle: "Instagram logo",
+    description: "Create catchy captions for your Instagram posts that increase engagement and followers.",
+  },
+  {
+    title: "LinkedIn Posts",
+    icon: "/icons/linkedin.png",
+    iconAltTitle: "LinkedInlogo",
+    description: "Craft professional content for your LinkedIn network to establish thought leadership.",
+  },
+];
+
+export const benefits = [
+  "Save time and effort on content creation",
+  "Consistently produce high-quality posts",
+  "Increase engagement across all platforms",
+  "Stay ahead of social media trends",
+  "Customize content to match your brand voice",
+  "Scale your social media presence effortlessly",
+];
