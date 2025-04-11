007a3039d77c080e905f7ac49bfcad8e90fd449e
marouane <marsou431@gmail.com>
2025-04-11 02:52:01 +0100

    get and update user points

---
+++ app/api/users/points/route.ts
@@ -0,0 +1,25 @@
+import { getUserPoints, updateUserPoints } from "@/drizzle/db/actions";
+
+export async function GET(req: Request) {
+  const requestUrl = new URL(req.url);
+  const user = requestUrl.searchParams.get("user");
+  
+  if (user === null) {
+    return new Response("No user param was found in the request", { status: 400 });
+  }
+
+  const points = await getUserPoints(user);
+  return Response.json({ points }, { status: 200 });
+}
+
+export async function PATCH(req: Request) {
+  const body = await req.json();
+  const { userId, newPoints } = body;  
+  
+  if (userId === null) {
+    return new Response("No user param was found in the request", { status: 400 });
+  }
+
+  const points = await updateUserPoints(userId, newPoints);
+  return Response.json({ points }, { status: 201 });
+}
