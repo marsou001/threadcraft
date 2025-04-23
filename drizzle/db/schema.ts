79883d1fe8cb7e43dafccf403327ecae48499f9d
marouane <marsou431@gmail.com>
2025-04-23 03:02:15 +0100

    adjust schema

--- drizzle/db/schema.ts
+++ drizzle/db/schema.ts
@@ -1,32 +1,36 @@
 import {
   pgTable,
-  integer,
   serial,
   text,
   timestamp,
   varchar,
-  boolean,
   pgEnum,
+  smallint,
 } from "drizzle-orm/pg-core";
 
 const socialMediaEnum = pgEnum("social_media", ["X", "Instagram", "LinkedIn"]);
+const toneEnum = pgEnum("tone", ["Casual", "Conversational", "Humorous", "Professional", "Empathetic", "Enthusiastic", "Authoritative", "Serious", "Neutral", "Joyful", "Friendly", "Encouraging"]);
 
 export const Users = pgTable("users", {
   id: serial("id").primaryKey(),
-  clerkId: text("clerkId").unique(),
-  email: text("email").notNull().unique(),
-  name: text("name"),
-  points: integer("points").default(50),
+  clerkId: varchar("clerkId", { length: 100 }).unique(),
+  email: varchar("email", { length: 50 }).notNull().unique(),
+  name: varchar("name", { length: 50 }),
+  points: smallint("points").default(50),
   createdAt: timestamp("created_at").defaultNow(),
 });
 
 export const GeneratedContent = pgTable("generated_content", {
   id: serial("id").primaryKey(),
-  userId: text("user_id")
-    .references(() => Users.id)
+  userId: varchar("user_id")
+    .references(() => Users.clerkId)
     .notNull(),
   content: text("content").notNull(),
   prompt: text("prompt").notNull(),
-  socialMedia: socialMediaEnum(),
+  tone: toneEnum("tone").notNull(),
+  numberOfHashtags: smallint("number_of_hashtags").notNull(),
+  socialMedia: socialMediaEnum("social_media").notNull(),
+  numberOfTweets: smallint("number_of_tweets"),
+  imagePrompt: text("image_prompt"),
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
