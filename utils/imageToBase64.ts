4dee0fde7c56d56c7c7284048548ace31be2d610
marouane <marsou431@gmail.com>
2025-04-24 02:05:05 +0100

    extract imageToBase64 to utils folder

---
+++ utils/imageToBase64.ts
@@ -0,0 +1,13 @@
+export default function imageToBase64(image: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = function() {
+      const base64ImagePrompt = reader.result as string;
+      resolve(base64ImagePrompt);
+    }
+    reader.onerror = function(error) {
+      reject(error);
+    }
+    reader.readAsDataURL(image);
+  })
+}
