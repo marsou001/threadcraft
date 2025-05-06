5584af6ab0d64f613ad49c91d6288d8a95d653df
marouane <marsou431@gmail.com>
2025-05-06 19:23:45 +0100

    add open graph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,19 @@ import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const baseUrl = "https://threadlyai.netlify.app";
+
 export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: "ThreadlyAI",
   description: "ThreadlyAI helps you craft engaging, AI-powered social media threads in seconds. Boost your content game with smart automation and creativity.",
   keywords: "AI social media, thread generator, AI threads, content creation, social media automation, Twitter threads, AI copywriting, AI content tool, ThreadlyAI",
+  openGraph: {
+    url: "/",
+    images: {
+      url: "/threadly.png",
+    }
+  }
 };
 
 export default async function RootLayout({
