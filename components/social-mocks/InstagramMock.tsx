897ca3acdfb5ad5585b483981ae25bfee3bf6d39
marouane <marsou431@gmail.com>
2025-05-07 00:54:46 +0100

    enhance instagram mockup UI

--- components/social-mocks/InstagramMock.tsx
+++ components/social-mocks/InstagramMock.tsx
@@ -1,5 +1,5 @@
-import { Heart, MessageCircle, Send, Bookmark } from "lucide-react";
 import Image from "next/image";
+import Actions from "./Actions";
 
 export default function InstagramMock({ content, image }: { content: string, image: File | null }) {
   const objectURL = image === null ? "" : URL.createObjectURL(image);
@@ -7,27 +7,23 @@ export default function InstagramMock({ content, image }: { content: string, ima
   return (
     <div className="text-black bg-white max-w-md mx-auto p-4 rounded-lg">
       <div className="flex items-center mb-3">
-        <div className="bg-gray-300 w-8 h-8 mr-3 rounded-full"></div>
-        <p className="font-bold">Your Name</p>
+        <div className="bg-gray-300 w-8 h-8 mr-1.5 rounded-full"></div>
+        <p className="text-sm font-semibold">your_name <span className="text-gray-400 font-normal">• 2d</span></p>
       </div>
       {objectURL.length === 0 ? (
-        <div className="bg-gray-200 overflow-hidden flex justify-center items-center h-64 mb-3">
+        <div className="bg-gray-200 overflow-hidden flex justify-center items-center h-64 mb-2 rounded-sm">
           <span className="text-gray-500">Image Placeholder</span>
         </div>
       ) : (
-        <div className="overflow-hidden flex justify-center items-center h-64 mb-3">
+        <div className="overflow-hidden flex justify-center items-center mb-2 rounded-sm">
           <Image src={objectURL} alt="Image" width={200} height={200} className="w-full" />
         </div>
       )}
-      <div className="flex justify-between mb-3">
-        <div className="flex space-x-4">
-          <Heart size={24} />
-          <MessageCircle size={24} />
-          <Send size={24} />
-        </div>
-        <Bookmark size={24} />
+      <div className="mb-1">
+        <Actions />
       </div>
-      <p className="text-sm">{content}</p>
+      <p className="text-md font-semibold">143 likes</p>
+      <p className="text-sm"><span className="font-semibold">your_name</span> {content}</p>
     </div>
   );
 };
